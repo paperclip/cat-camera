3c3dc5f6622f8e51eaf695f9e2103ff8ad4f6834
paperclip <douglas.leeder@gmail.com>
2018-09-25 19:39:25 +0100

    Use new model and directories.

--- ClassifyNewImages.py
+++ ClassifyNewImages.py
@@ -4,28 +4,29 @@ import os
 import shutil
 import subprocess
 
+## rsync -va douglas@pi:webdata/camera/ camera
+
 RSYNC=r"F:\cygwin64\bin\rsync"
 
 os.chdir(r"C:\Users\windo\Documents\camera")
 
 # subprocess.call([RSYNC,"-va","douglas@pi:webdata/camera/","camera"])
 
-cats = set(os.listdir("cat"))
-notcats = set(os.listdir("not_cat"))
+catDir = os.path.join("images","cat")
+
+cats = set(os.listdir(catDir))
+notcats = set(os.listdir(os.path.join("images","not_cat")))
 camera = set(os.listdir("camera"))
 
-print(len(cats))
-print(len(notcats))
-print(len(camera))
+assert len(cats) > 0
+assert len(notcats) > 0
+
+print("Cat images %d"%len(cats))
+print("Not cat images %d"%len(notcats))
+print("Total images %d"%len(camera))
 
 new = camera - cats
 new -= notcats
-
-marker = "timelapse-2018-08-12-13-15-24.jpeg"
-
-new = [ n for n in new if n > marker ]
-
-new = sorted(new)
 
 def safemkdir(d):
     try:
@@ -34,21 +35,33 @@ def safemkdir(d):
         pass
 
 for i in range(10):
-    safemkdir("new_cat/%d0"%i)
+    p = os.path.join("new_cat","%d0"%i)
+    safemkdir(p)
+    new -= set(os.listdir(p))
 
+marker = "timelapse-2018-08-12-13-15-24.jpeg"
 
-import image_classify
-c = image_classify.ImageClassify(['cat', 'not_cat'], image_size=100, learning_rate=0.001)
-c.load_model('cat_water')
+new = [ n for n in new if n > marker ]
 
-print(len(new))
+new = sorted(new)
+
+print("New images %d"%(len(new)))
+
+import label_image as classifier
+c = classifier.ImageClassify()
+
+## ['cat', 'not_cat'], image_size=100, learning_rate=0.001)
+## c.load_model('cat_water')
+
+remaining = len(new)
 
 for n in new:
     src = os.path.join("camera",n)
     results = c.predict_image(src)
     dest = os.path.join("new_cat","%d0"%(int(results[1][0]*10)))
-    print(src,results,dest)
+    print("%s\t%s\t%s\t%d"%(src,str(results),dest,remaining))
     shutil.copy(src,dest)
+    remaining -= 1
 
 
 
