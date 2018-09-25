5fbac3b28a63851d6ad07b3465e5829726de2de6
paperclip <douglas.leeder@gmail.com>
2018-09-25 19:39:55 +0100

    Play with different batch sizes

--- training.py
+++ training.py
@@ -11,11 +11,11 @@ not_cat_images = glob.glob('not_cat/*.jpeg')
 assert(len(not_cat_images) > 10)
 
 selected_not_cat_images = [ x for x in not_cat_images if os.path.getsize(x) > 80 * 1024 ]
-selected_not_cat_images = random.sample(selected_not_cat_images, len(cat_images) * 2)
+selected_not_cat_images = random.sample(selected_not_cat_images, len(cat_images) * 1)
 
 images = cat_images + selected_not_cat_images
 
-assert(len(images) == 3 * len(cat_images))
+assert(len(images) == 2 * len(cat_images))
 
 print("%d cat images"%len(cat_images))
 print("%d not_cat images"%len(selected_not_cat_images))
