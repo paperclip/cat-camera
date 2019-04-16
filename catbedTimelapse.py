8b2a471bfdeb72db1fad4e59ff7dc789b00780d1
paperclip <douglas.leeder@gmail.com>
2019-04-16 22:05:16 +0100

    Allow specification of different targets
    Allow timelapses of different directories.

--- catbedTimelapse.py
+++ catbedTimelapse.py
@@ -1,17 +1,28 @@
 #!/bin/env python
 ## from 2018-10-21 08:30:09
 import os
+import sys
 
 import timelapse
 
-files = []
-tempdir = "catTemp"
-srcdir = "cat"
+def main(argv):
+    if len(argv) > 1:
+        srcdir = argv[1]
+    else:
+        srcdir = "cat"
 
-for f in os.listdir(srcdir):
-    files.append(f)
+    if len(argv) > 2:
+        dest = argv[2]
+    else:
+        dest = "cat_timelapse.mp4"
 
-files.sort()
+    files = os.listdir(srcdir)
+    files.sort()
 
-i = timelapse.linkIntoTemp(files, srcdir, tempdir, 1000)
-timelapse.createVideo("cat_timelapse.mp4", tempdir)
+    tempdir = "timelapseTemp"
+
+    i = timelapse.linkIntoTemp(files, srcdir, tempdir, 1000)
+    timelapse.createVideo(dest, tempdir)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
