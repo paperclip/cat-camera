9373a853352aaa45d03a8dff62412a3634893191
paperclip <douglas.leeder@gmail.com>
2019-11-16 17:20:42 +0000

    Handle move1000 for non_cat

--- move1000.py
+++ move1000.py
@@ -4,8 +4,8 @@ import os
 import sys
 
 imagesdir = "images"
-catdir = os.path.join(imagesdir, "cat")
-destdir = os.path.join(imagesdir, "cat1000")
+catdir = os.path.join(imagesdir, "not_cat")
+destdir = os.path.join(imagesdir, "1000")
 
 files = os.listdir(catdir)
 files.sort()
@@ -13,9 +13,13 @@ files.sort()
 files = files[:1000]
 assert len(files) == 1000
 
+try:
+    os.makedirs(destdir)
+except EnvironmentError:
+    pass
+
 for f in files:
     src = os.path.join(catdir, f)
     dest = os.path.join(destdir, f)
     os.rename(src, dest)
     print(src, dest)
-    
